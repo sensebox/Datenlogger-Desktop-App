554f4cdc5f7ce4129cbaa17c06c8e10fc95d6931
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-06-09 12:12:13 +0200

    Init react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
+import Contact from "./routes/contact";
+
 import "./styles.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
